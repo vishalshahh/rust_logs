bb2a64a8e484da0c2fe082b8bd93b49031f433b3
vishalshahh <vishal12kl@gmail.com>
2025-10-01 17:39:51 +0530

    feat: learned strings and tuples

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,27 @@ fn main() {
 
     // Creating a string_literal
     let my_string_literal = "Vishal is a software engineer";
-    println!("{}", my_string_literal)
+    println!("{}", my_string_literal);
+
+    let _new_string: String = String::from("Hello World!"); //immutable string
+
+    let mut s = String::from("Hello");
+    s.push_str(", world!"); // push_str appends a literal to a String
+    println!("{}", s); // this will print "Hello, world!"
+
+    //Tuples
+
+    let employee_info: (&str, i32) = ("Salary", 50_000);
+    println!("{} is equal to {}", employee_info.0, employee_info.1);
+    println!("The complete tuple at once {:?}", employee_info);
+
+    let (salary, salary_values) = employee_info; // Destructuring
+    println!("{} of the employee is {}", salary, salary_values);
+
+    let salary = employee_info.0;
+    let salary_values = employee_info.1;
+    println!("{} of the employee is {}", salary, salary_values);
+
 }
 
 
